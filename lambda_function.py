d8def64b0bac102e22c356ebe735aa36619b7839
brogleyr <ryanbrogley@gmail.com>
2025-08-01 15:15:11 -0400

    fixed client initialization

--- lambda_function.py
+++ lambda_function.py
@@ -2,8 +2,7 @@ import json
 import boto3
 
 def lambda_handler(event, context):
-
-    s3vector = boto3.client('s3vector', region_name='us-east-1')
+    s3vector = boto3.client('s3vectors', region_name='us-east-1')
     
     response = s3vector.get_vectors(
         vectorBucketName='dev-nc-job-embeddings',
