3813df23601bc51f01140485b172054b4f6b9bd8
brogleyr <ryanbrogley@gmail.com>
2025-08-01 12:17:28 -0400

    added test case

--- test/test_lambda_function.py
+++ test/test_lambda_function.py
@@ -1,10 +1,13 @@
-from lambda_function import lambda_handler
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import lambda_function
 
 
 def test_lambda_handler():
     event = {}
     context = {}
-    response = lambda_handler(event, context)
+    response = lambda_function.lambda_handler(event, context)
     
     assert response['status'] == 200
     assert 'body' in response
